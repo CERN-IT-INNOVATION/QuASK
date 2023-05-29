6aff8fd9d961ee82a51020bbe48e13fd8962ab8e
Francesco Di Marcantonio <francesco.di.marcantonio@cern.ch>
2023-05-29 21:11:39 +0200

    license adjustment

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     description='Quantum Advantage Seeker withKernels',
     url='https://github.com/CERN-IT-INNOVATION/QuASK',
     author='Massimiliano Incudini <massimiliano.incudini@univr.it>, Francesco Di Marcantonio <francesco.di.marcantonio@cern.ch>, Michele Grossi <michele.grossi@cern.ch>',
-    license='Apache License Version 2.0',
+    license='Apache Software License',
     packages=setuptools.find_packages(),
     python_requires='>=3.7.0',
     install_requires=[
